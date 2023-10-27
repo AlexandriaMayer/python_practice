48923760cb604154ada7da2b9aff84ab3d03ea9a
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2023-10-27 15:57:13 -0500

    Update pandas_practice.py
    "2. Data Manipulation
    
    a. Sort the DataFrame based on the Age column in descending order.
    
    b. Create a new column called Senior which marks True if the age is above 60, and False otherwise.
    
    c. Replace a city name of your choice with another city name for all occurrences."
    
    Done. Took less than an hour of searching and debugging code.

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,23 +1,20 @@
+# I asked ChatGPT to give me exercises to practice and learn Pandas. 
+# I'll update this as I complete the lessons it assigned me.
+
 import pandas as pd
+import numpy as np
 
 # creating the data
 
-#data = [["Hazel", "Tom", "Ulysses", "Sarah", "Daniel"], [21, 31, 24, 20, 25], ["Austin", "Baton Rouge", "San Marcos", "Cedar Park", "Dripping Springs"]]
 data = [["Hazel", 21, "Austin"], ["Tom", 36, "Kyle"], ["Ulysees", 26, "San Marcos"], ["Sarah", 21, "Austin"], ["Daniel", 25, "Austin"]]
 
 # creating the dataframe
 
 df = pd.DataFrame(data, columns = ["name", "age", "city"])
 
-# print dataframe
-
-# print(df)
-
-# getting the first 3 rows from df
+# get and print the first 3 rows from df
 
 df_first_3 = df.head(3)
-
-# print the first 3
 
 print(df_first_3)
 
@@ -27,6 +24,15 @@ df_last_2 = df.tail(2)
 
 print(df_last_2)
 
-# filter for ages greater than 25
+# filter and print for ages greater than 25
 
 print(df[df['age'] > 25])
+
+# sort by age, add senior field, change city name for Ulysses.
+
+print(df.sort_values("age", ascending=False))
+
+df["senior"] = np.where(df['age'] > 60, True, False)
+
+df.loc[df['city'] == 'Kyle', 'city'] = 'Austin'
+print(df)
