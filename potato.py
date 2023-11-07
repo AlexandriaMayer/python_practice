d65fc6a88272fe2d2ec711fd7fd2716bc84ac8b3
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2023-11-06 22:13:39 -0600

    Update potato.py

--- potato.py
+++ potato.py
@@ -1,4 +1,10 @@
-potatoes = ['russet', 'golden', 'red']
+import pandas as pd
+
+potatoes = ['russet', 'golden', 'red', 'potato']
 
 for potato in potatoes:
     print(potato)
+
+# I don't know why I did this. I think I wanted potatoes, and I wanted to memorize python code. My head kept repeating "for potato in potato", and I figured I should see if
+# that's a real loop. Hey! A For loop! Amazing!
+# What I want to is: select 'potato' from the list, but only select characters.
