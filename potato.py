edc652963a415b5e8b6ca99159a3ad50cb2a8c7a
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2023-11-06 22:14:02 -0600

    Update potato.py

--- potato.py
+++ potato.py
@@ -5,6 +5,6 @@ potatoes = ['russet', 'golden', 'red', 'potato']
 for potato in potatoes:
     print(potato)
 
-# I don't know why I did this. I think I wanted potatoes, and I wanted to memorize python code. My head kept repeating "for potato in potato", and I figured I should see if
-# that's a real loop. Hey! A For loop! Amazing!
+# I don't know why I did this. I think I wanted potatoes, and I wanted to memorize python code. My head kept repeating 
+# "for potato in potato", and I figured I should see if that's a real loop. Hey! A For loop! Amazing!
 # What I want to is: select 'potato' from the list, but only select characters.
